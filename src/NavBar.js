06065b9e6f48912674627200472c9ca505b27357
Priscilla S <priscilla.lee.son@gmail.com>
2023-03-07 02:39:08 +0000

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,12 @@
 export const NavBar = () => {
   return (
     <div className="navbar-list">
-      <div className="vocabulary-logo">Vocabulary</div>
-      <div className="contact-link">Contact</div>
+      <div className="vocabulary-logo">
+        <a href="#">Vocabulary</a>
+      </div>
+      <div className="contact-link">
+        <a>Contact</a>
+      </div>
     </div>
   );
 };
